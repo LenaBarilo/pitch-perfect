b749cb026c353af554de9394c3e83faa64a39ca7
LenaBarilo <lena3432003@yahoo.com>
2015-04-02 10:33:21 -0700

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,32 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Yelena Barilo on 3/27/15.
+//  Copyright (c) 2015 Yelena Barilo. All rights reserved.
+//
+
+import UIKit
+
+class RecordedAudio: NSObject {
+
+    var filePathURL: NSURL!
+    var title: String!
+    
+    override init() {
+        filePathURL = nil
+        title = nil
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
